654715cb81f1a1042166db1e9b2a1af05b9d4e8b
Tibor Szalay <core966@indamail.hu>
2015-06-03 15:45:45 +0000

    Corrected logic, polished look of post view

--- posts_controller.rb
+++ posts_controller.rb
@@ -5,30 +5,34 @@
     end
     
     get '/posts/new' do
-	@title = @title + " | New Post"
-	@post = Post.new
-	erb "post_views/new_post".to_sym
+	    @title = @title + " | New Post"
+  	  @post = Post.new
+	    erb "post_views/new_post".to_sym
     end
     
     get '/posts/:id' do
-      @post = Post.find_by_sql("SELECT posts.* FROM posts WHERE posts.id = " + params[:id])
+      @post = Post.find_by_sql(["SELECT posts.* FROM posts WHERE posts.id = ? AND posts.is_deleted = false", params[:id]])
     
-      if @post[0].is_deleted == true #We check if the post is already flagged as deleted.
+      if @post[0].nil? == true #We check if the post is already flagged as deleted.
         redirect "/posts"
       end
 
-      @post = Post.find_by_sql("SELECT posts.*, comments.* FROM posts, comments WHERE comments.post_id = posts.id AND posts.id = " + params[:id] + " AND comments.is_deleted = 0") #We get the post and the comment of it.
-      @comment = Comment.find_by_sql("SELECT posts.*, comments.* FROM posts, comments WHERE comments.post_id = posts.id AND posts.id = " + params[:id] + " AND comments.was_expanded = 1 AND comments.is_deleted = 0") #We check if the comment is already expanded.
-      if (nil == @post[0]) #But if there is no result...
-	@post = Post.find_by_sql("SELECT * FROM posts WHERE posts.id = " + params[:id]) #...then there is no comment for the post.
+      @post = Post.find_by_sql(["SELECT posts.*, comments.* FROM posts, comments WHERE comments.post_id = posts.id AND posts.id = ? AND comments.is_deleted = 0", params[:id]]) #We get the post and the comment of it.
+      @comment = Comment.find_by_sql(["SELECT posts.*, comments.* FROM posts, comments WHERE comments.post_id = posts.id AND posts.id = ? AND comments.is_deleted = 0", params[:id]]) #We check if the comment is already expanded.
+      if (@post[0].nil?) #But if there is no result...
+	      @post = Post.find_by_sql(["SELECT * FROM posts WHERE posts.id = ?", params[:id]]) #...then there is no comment for the post.
         @no_comment = true #We need to tell it to the erb template also, because rendering is different.
       end
-      if (nil == @comment[0])
-        @not_expanded = true #We will manipulate the view if the comment is already expanded (it will be no longer editable).
+      if (@comment[0].nil? == false)
+        if (@comment[0].was_expanded == true)
+          @expanded = true #We will manipulate the view if the comment is already expanded (it will be no longer editable).
+        else
+          @expanded = false #We will manipulate the view if the comment is already expanded (it will be no longer editable).
+        end
       end
       @comment = Comment.new #We need to prepare here the new comment because the comments are on the show page of the post.
       @title = @title + " | " + @post[0].title
-      erb "post_views/show_post".to_sym
+      erb "post_views/show_post_v2".to_sym
     end
     
     post '/posts/?' do
@@ -53,12 +57,12 @@
 
     post '/comments' do
       @comment = Comment.new(params[:comment])
-	if @comment.save
-	  redirect "/posts/#{@comment.post_id}"
-	else
-	  flash[:error] = "There is problem with the save of the comment!"
-	  redirect "/posts/"
-	end
+    	if @comment.save
+    	  redirect "/posts/#{@comment.post_id}"
+    	else
+    	  flash[:error] = "There is problem with the save of the comment!"
+    	  redirect "/posts/"
+    	end
     end
 
     put '/comments/:id/edit' do #Don't forget that this method will only append content, and only once at most!
@@ -67,11 +71,11 @@
       
       expanded_comment = params[:comment][:comment] + "\n\n" + "Edit:" + "\n\n" + params[:comment][:expansion]
       
-	if comment.update_attributes(comment: expanded_comment, was_expanded: true)
-	  redirect "/posts/#{params[:comment][:post_id]}"
-	else
-	  redirect "/posts/#{params[:id]}"
-	end
+    	if comment.update_attributes(comment: expanded_comment, was_expanded: true)
+    	  redirect "/posts/#{params[:comment][:post_id]}"
+    	else
+    	  redirect "/posts/#{params[:id]}"
+    	end
  
     end
 
@@ -83,9 +87,10 @@
 
       @post_id = @post_id[0].post_id.to_s
       
-	if comment.update_attributes(params[:comment])
-	  redirect "/posts/#{@post_id}"
-	else
-	  redirect "/posts/#{params[:id]}"
-	end
-   end
+    	if comment.update_attributes(params[:comment])
+    	  redirect "/posts/#{@post_id}"
+    	else
+    	  redirect "/posts/#{params[:id]}"
+    	end
+    	
+    end
